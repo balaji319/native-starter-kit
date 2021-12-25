49f47f69f816b2446072b20d818beeeb51cb5d2e
Admn <admn@Admns-MacBook-Air.local>
2021-12-25 11:11:24 +0530

    initial commit

---
+++ native/Shared/nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  nativeApp.swift
+//  Shared
+//
+//  Created by Admn on 25/12/21.
+//
+
+import SwiftUI
+
+@main
+struct nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
